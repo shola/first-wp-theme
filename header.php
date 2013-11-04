371d6c4a375cfcf024183edc2818abafeda35643
Mike Situ <michael.o.situ@gmail.com>
2013-11-04 13:43:13 -0800

    added some dynamic meta tags to header

--- header.php
+++ header.php
@@ -5,16 +5,18 @@
     <meta charset=<?php bloginfo('charset') ?>>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="keywords" content "situ, mike situ, michael situ, michael olusola situ, stanford, san francisco, engineer, mobile, web, silicon valley, high tech, tech">
-    <meta name="description" content="Mike Situ is a software engineer, currently living in San Francisco, CA. This homepage is a showcase for his current projects, and gives an easy way to connect with him.">
-    <meta name="author" content="Mike Situ">
+    <meta name="description" content="<?php bloginfo('description'); ?>">
+    <meta name="author" content="<?php get_the_author(); ?>">
     <link rel="shortcut icon" href="http://files.softicons.com/download/internet-icons/black-social-media-icons-by-clay-cauley/png/24/ccinc_reddit.png">
-    <title>Mike Situ's Homepage</title>
+    <title><?php wp_title('|', true, 'right'); ?>
+           <?php bloginfo('name'); ?></title>
 
-    <!-- Bootstrap core CSS -->
+    <!-- WP styles, including Bootstrap core CSS -->
+<!--     <link rel="stylesheet" href="<?php bloginfo('stylesheet'); ?>"> -->
     <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
     <!-- <link href="css/bootstrap.css" rel="stylesheet"> -->
-
-    <!-- Custom styles for this template -->
+ 
+    <!-- Custom sitewide styles for this template -->
     <link href="../../../../css/styles.css" rel="stylesheet">
 
     <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
@@ -25,7 +27,7 @@
   </head>
 
   <body>
-    <div id="wrap">
+    <div id="wrap simple-style">
       <div class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
           <div class="navbar-header">
@@ -52,3 +54,4 @@
           </div><!--/.nav-collapse -->
         </div>
       </div> <!-- end nav bar -->
+    <hr/> <!-- temporary fix to get rid of overlap -->
