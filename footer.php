7007b3a98024e6223006076da147720a94c8a49c
Mike Situ <michael.o.situ@gmail.com>
2013-11-04 14:04:37 -0800

    added links to footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+		<footer id="footer">
+	     <p class="top-link-footer"><a href="#top"><?php _e('Go To Top', 'mike-framework'); ?></a></p>
+       <p class="text-center muted">&copy Copyright <?php echo date('Y'); ?> <a href="<?php echo home_url(); ?>"><?php bloginfo('name') ?></a></p>
+      <!-- </div>  --> 
+    </footer>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <!-- <script src="http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.3.0/respond.js"></script> -->
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+    <!-- <script src="js/jquery.js"></script>
+    <script src="js/bootstrap.min.js"></script> -->
+ 
+</body></html>
