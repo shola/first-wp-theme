e078ea25dd9422add5f3468b7e0590b08fd23a35
Mike Situ <michael.o.situ@gmail.com>
2013-11-04 14:18:33 -0800

    added top & main menus

--- functions.php
+++ functions.php
@@ -1,8 +1,14 @@
 <?php 
 	
-define('THEMEROOT', get_stylesheet_directory_uri());
-define('IMAGES', THEMEROOT . '/images');
+	define('THEMEROOT', get_stylesheet_directory_uri());
+	define('IMAGES', THEMEROOT . '/images');
 
-
+	function register_my_menus() {
+		register_nav_menus(array(
+			'top-menu' => __('Top Menu', 'mike-framework'),
+			'main-menu' => __('Main Menu', 'mike-framework')
+		));
+	}
+	add_action('init', 'register_my_menus');
 
 ?>
