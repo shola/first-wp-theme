95a773bad4f6999597b7b97afb76e0f04fbf228c
Mike Situ <michael.o.situ@gmail.com>
2013-11-04 13:15:53 -0800

    added static navbar (non-functioning)

---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <meta charset=<?php bloginfo('charset') ?>>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content "situ, mike situ, michael situ, michael olusola situ, stanford, san francisco, engineer, mobile, web, silicon valley, high tech, tech">
+    <meta name="description" content="Mike Situ is a software engineer, currently living in San Francisco, CA. This homepage is a showcase for his current projects, and gives an easy way to connect with him.">
+    <meta name="author" content="Mike Situ">
+    <link rel="shortcut icon" href="http://files.softicons.com/download/internet-icons/black-social-media-icons-by-clay-cauley/png/24/ccinc_reddit.png">
+    <title>Mike Situ's Homepage</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="http://getbootstrap.com/dist/css/bootstrap.css" rel="stylesheet">
+    <!-- <link href="css/bootstrap.css" rel="stylesheet"> -->
+
+    <!-- Custom styles for this template -->
+    <link href="../../../../css/styles.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+    <div id="wrap">
+      <div class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+          <div class="navbar-header">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+              <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="">Mike Situ</a>
+          </div>
+          <div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+              <li class="drowndown">
+                <a href="" class="dropdown-toggle" data-toggle="dropdown">Projects <b class="caret"></b></a>
+                <ul class="dropdown-menu">
+                  <li><a href="app.html">r/Funny App</a></li>
+                </ul>
+                </a>
+              </li>
+              <li class="active"><a href="index.html">Home</a></li>
+              <li><a href="contact.html">Contact</a></li>
+              <li><a href="about.html">About</a></li>
+            </ul>
+          </div><!--/.nav-collapse -->
+        </div>
+      </div> <!-- end nav bar -->
