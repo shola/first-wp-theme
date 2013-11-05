7876016bbc964aaef58415138c8b0126aafdf8ab
Mike Situ <michael.o.situ@gmail.com>
2013-11-05 14:39:58 -0800

    basic search form

---
+++ searchform.php
@@ -0,0 +1,6 @@
+<form action="<?php	echo home_url(); ?>" id="search-form" method="get">
+						 
+	 <input type="text" value="search" name="s" id="s" onblur="if(this.value=='')this.value='search'" onfocus="if(this.value=='search')this.value=''" />
+	 <input type="hidden" value="submit" />
+	 
+ </form>
