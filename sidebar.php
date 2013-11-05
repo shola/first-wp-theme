b5aa175b00a3eb60e42d11d112300742662b298f
Mike Situ <michael.o.situ@gmail.com>
2013-11-05 12:52:08 -0800

    sidebar stub

--- sidebar.php
+++ sidebar.php
@@ -1,1 +1,5 @@
-<p>this will be the sidebar</p>
+<!-- if dynamic_sidebar doesn't exist, or you cannot display the main side bar -->
+
+<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('main-sidebar')) : ?>
+		no widgets here
+<?php endif; ?>
