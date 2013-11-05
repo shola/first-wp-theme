a9f87680cec9fb8070233e9117cea401bad30f66
Mike Situ <michael.o.situ@gmail.com>
2013-11-05 12:40:43 -0800

    registered footer widget

--- functions.php
+++ functions.php
@@ -11,12 +11,23 @@
 	}
 	add_action('init', 'register_my_menus');
 
-	if (function_exists('register_sidebar')) {}
+	if (function_exists('register_sidebar')) {
 		register_sidebar(
 			array(
 				'name' => __('Main Sidebar', 'mike-framework'),
 				'id' => 'main-sidebar',
 				'description' => __('The main sidebar area', 'mike-framework'),
+				'before_widget' => '<div class="sidebar-widget">',
+				'after_widget' => '</div> <!-- end sidebar-widget -->',
+				'before_title' => '<h4>',
+				'after_title' => '</h4>'
+		));
+
+		register_sidebar(
+			array(
+				'name' => __('Left Footer', 'mike-framework'),
+				'id' => 'left-footer',
+				'description' => __('The left footer area', 'mike-framework'),
 				'before_widget' => '<div class="sidebar-widget">',
 				'after_widget' => '</div> <!-- end sidebar-widget -->',
 				'before_title' => '<h4>',
