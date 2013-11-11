56a2ebfbf05fa5418d8773e037b510749d92dc79
Mike Situ <michael.o.situ@gmail.com>
2013-11-11 15:59:41 -0800

    esc_attr calls were working, posts just had to be approved in dash

--- functions.php
+++ functions.php
@@ -134,15 +134,15 @@
 		// esc_attr (escape attributes) automatically encodes special characters from strings to HTML codes
 		$fields = array(
 			'author' => '<p>' . 
-										'<input type="text" name="author" id="author" value="' . $commenter['comment_author'] . '" ' . $aria_req .' />' .
+										'<input type="text" name="author" id="author" value="' . esc_attr($commenter['comment_author']) . '" ' . $aria_req .' />' .
 										'<label for="author">' . __('Name', 'mike-framework') . ' ' . ($req ? '*' : '') . '</label>' . 
 									 '</p>',
 			'email' => '<p>' . 
-										'<input type="text" name="email" id="email" value="' . $commenter['comment_author_email'] . '" ' . $aria_req .' />' .
+										'<input type="text" name="email" id="email" value="' . esc_attr($commenter['comment_author_email']) . '" ' . $aria_req .' />' .
 										'<label for="email">' . __('Email', 'mike-framework') . ' ' . ($req ? '*' : '') . '</label>' . 
 									 '</p>',
 			'url' => '<p>' . 
-										'<input type="text" name="url" id="url" value="' . $commenter['comment_author_url'] . '" />' .
+										'<input type="text" name="url" id="url" value="' . esc_attr($commenter['comment_author_url']) . '" />' .
 										'<label for="url">' . __('Website', 'mike-framework') . '</label>' . 
 									 '</p>'
 		);
