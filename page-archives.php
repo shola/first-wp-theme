dec717b0285adff93fac3e507b1c23e2f3ee6d5c
Mike Situ <michael.o.situ@gmail.com>
2013-11-11 16:43:56 -0800

    added monthly archive items

---
+++ page-archives.php
@@ -0,0 +1,55 @@
+<?php 
+/*
+	Template Name: Archives Page
+*/
+
+?>
+
+<?php get_header(); ?>
+
+	<!-- MAIN CONTENT -->
+	<div class="container">
+	
+		<div class="row">
+		
+			<div class="col-sm-9 col-md-9 article-container-fix">
+				
+				<div class="articles">
+				
+					<?php if(have_posts()) : while(have_posts()) : the_post();  ?>
+					<article class="clearfix">
+						
+						<header>
+							
+							<h1><?php the_title(); ?></a></h1>
+							<?php if(current_user_can('edit_post', $post->ID)) {
+																// BEFORE                         AFTER
+								edit_post_link(__('Edit This', 'mike-framework'), '<p class="article-meta-extra">',	'</p>'); 
+							}?>
+						
+						</header>
+
+						<hr class="image-replacement"/>
+						
+						<h4><?php _e('Archives by Month', 'mike-framework'); ?></h4>
+						<ul>
+						    <?php wp_get_archives('type=monthly'); ?>
+						</ul>
+						
+					</article>
+
+				</div> <!-- end articles -->
+				
+			</div> <!-- end span9 -->
+			
+			<aside class="col-sm-3 col-md-3 main-sidebar">
+				
+				<?php get_sidebar(); ?>
+
+			</aside>
+			
+		</div> <!-- end row -->
+		
+	</div> <!-- end container --> 
+
+<?php get_footer(); ?>
