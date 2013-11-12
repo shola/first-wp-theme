99e7629ac5230c04a05164350f816e4752bdc5c9
Mike Situ <michael.o.situ@gmail.com>
2013-11-12 11:58:41 -0800

    page not found message and search form added

--- 404.php
+++ 404.php
@@ -9,55 +9,23 @@
 				
 				<div class="articles">
 				
-					<?php if(have_posts()) : while(have_posts()) : the_post();  ?>
 					<article class="clearfix">
 						
 						<header>
 							
-							<h1><?php the_title(); ?></a></h1>
-							<?php if(current_user_can('edit_post', $post->ID)) {
-																// BEFORE                         AFTER
-								edit_post_link(__('Edit This', 'mike-framework'), '<p class="article-meta-extra">',	'</p>'); 
-							}?>
+							<h1><?php _e('Page not found!', 'mike-framework'); ?></a></h1>
 						
 						</header>
 
 						<hr class="image-replacement"/>
-						
-						<?php the_content(); ?>
-						
-						<div>
 
-							<!-- Here are 2 ways to accomplish page navigation -->
-							
-							<!-- <?php wp_link_pages('before=<p class="post-navigation">&after="</p>"&pagelink=Page %'); ?> -->
+						<?php _e("Oops, it seems you're looking for something that's not here. Please try again.", 'mike-framework'); ?>
 
-							<?php $args = array(
-								'before' => '<p class="post-navigation">',
-								'after' => "</p>",
-								'pagelink' => 'Page %'
-							); ?>
-							<?php wp_link_pages($args); ?>
-							
-						</div> <!-- end post-navigation -->
+						<?php get_search_form(); ?>
 						
 					</article>
-
-				<?php endwhile; else : ?>
-
-					<article>
-						<h1><?php _e('No posts were found', 'mike-framework'); ?></h1>
-					</article>
-
-				<?php endif; ?>
 					
 				</div> <!-- end articles -->
-				
-				<div class="comments-area" id="comments">
-					
-					<?php comments_template('', true); ?>
-					
-				</div> <!-- end comments-area -->
 				
 			</div> <!-- end span9 -->
 			
